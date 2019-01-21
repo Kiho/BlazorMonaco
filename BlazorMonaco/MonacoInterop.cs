fb45d0fea6ccff1bef253697978faf1e405e079c
Kiho <kihoc@gmail.com>
2019-01-20 20:49:30 -0800

    Rename parameter to id

--- BlazorMonaco/MonacoInterop.cs
+++ BlazorMonaco/MonacoInterop.cs
@@ -5,13 +5,13 @@ namespace BlazorMonaco
 {
 	public static class MonacoInterop
     {
-		public static Task<bool> EditorInitialize(string editorModel, string script, string language)
-			=> JSRuntime.Current.InvokeAsync<bool>("monacoJsFunctions.EditorInitialize", new[] { editorModel, script, language });
+		public static Task<bool> EditorInitialize(string id, string script, string language)
+			=> JSRuntime.Current.InvokeAsync<bool>("monacoJsFunctions.EditorInitialize", new[] { id, script, language });
 
-		public static Task<EditorModel> EditorGet(string editorModel)
-			=> JSRuntime.Current.InvokeAsync<EditorModel>("monacoJsFunctions.EditorGet", new[] { editorModel });
+		public static Task<EditorModel> EditorGet(string id)
+			=> JSRuntime.Current.InvokeAsync<EditorModel>("monacoJsFunctions.EditorGet", new[] { id });
 
-        public static Task<EditorModel> EditorSet(string editorModel, string script)
-            => JSRuntime.Current.InvokeAsync<EditorModel>("monacoJsFunctions.EditorSet", new[] { editorModel, script });
+        public static Task<EditorModel> EditorSet(string id, string script)
+            => JSRuntime.Current.InvokeAsync<EditorModel>("monacoJsFunctions.EditorSet", new[] { id, script });
     }
 }
